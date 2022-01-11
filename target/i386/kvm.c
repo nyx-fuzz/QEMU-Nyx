213c614124dac904364993279c20008529cb4410
Sergej Schumilo <sergej@schumilo.de>
2022-01-11 03:01:20 +0100

    remove dead code (kvm.c)

--- target/i386/kvm.c
+++ target/i386/kvm.c
@@ -3808,27 +3808,6 @@ static int kvm_guest_debug_workarounds(X86CPU *cpu)
     return ret;
 }
 
-static int kvm_put_debugregs(X86CPU *cpu)
-{
-    CPUX86State *env = &cpu->env;
-    struct kvm_debugregs dbgregs;
-    int i;
-
-    if (!kvm_has_debugregs()) {
-        return 0;
-    }
-
-    memset(&dbgregs, 0, sizeof(dbgregs));
-    for (i = 0; i < 4; i++) {
-        dbgregs.db[i] = env->dr[i];
-    }
-    dbgregs.dr6 = env->dr[6];
-    dbgregs.dr7 = env->dr[7];
-    dbgregs.flags = 0;
-
-    return kvm_vcpu_ioctl(CPU(cpu), KVM_SET_DEBUGREGS, &dbgregs);
-}
-
 static int kvm_get_debugregs(X86CPU *cpu)
 {
     CPUX86State *env = &cpu->env;
