e82923f7de91ab15d40eaaaead64ea6fd2ea1600
Steffen Schulz <steffen.schulz@intel.com>
2022-05-22 22:44:59 +0200

    redirect Nyx logging to qemu_log() facility
    Redirect all custom logging macros to use qemu_log() and qemu_log_mask().
    
    Default qemu_log is written to stderr or logged with -D <file>
    Additional qemu_log_masked(LOG_KAFL) can be enabled with -d kafl

--- nyx/debug.h
+++ nyx/debug.h
@@ -5,6 +5,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "qemu/osdep.h"
+#include "qemu-common.h"
+#include "qemu/log.h"
+
 #define ENABLE_BACKTRACES
 
 #define QEMU_PT_PRINT_PREFIX  "[QEMU-PT]\t"
@@ -27,11 +31,27 @@
 
 
 #ifdef NYX_VERBOSE
+//#define NYX_TRACELOG // enable logging via qemu -D and -trace kafl facilities
+#ifndef NYX_TRACELOG
 #define debug_printf(format, ...) printf (format, ##__VA_ARGS__)
 #define debug_fprintf(fd, format, ...) fprintf (fd, format, ##__VA_ARGS__)
 #define QEMU_PT_PRINTF(PREFIX, format, ...) printf (QEMU_PT_PRINT_PREFIX COLOR PREFIX format ENDC "\n", ##__VA_ARGS__)
 #define QEMU_PT_PRINTF_DBG(PREFIX, format, ...) printf (QEMU_PT_PRINT_PREFIX PREFIX "(%s#:%d)\t"format, __BASE_FILE__, __LINE__, ##__VA_ARGS__)
 #define QEMU_PT_PRINTF_DEBUG(format, ...)  fprintf (stderr, QEMU_PT_PRINT_PREFIX DEBUG_VM_PREFIX "(%s#:%d)\t"format "\n", __BASE_FILE__, __LINE__, ##__VA_ARGS__)
+#else
+/*
+ * qemu_log() is the standard logging enabled with -D
+ * qemu_log_mask() is activated with additional -t kafl option
+ * Some of these are actual errors that should go to stderr..
+ */
+#define debug_printf(format, ...)               qemu_log_mask(LOG_KAFL, QEMU_PT_PRINT_PREFIX DEBUG_VM_PREFIX "(%s#:%d)\t"format, __BASE_FILE__, __LINE__, ##__VA_ARGS__)
+#define debug_fprintf(fd, format, ...)          qemu_log_mask(LOG_KAFL, QEMU_PT_PRINT_PREFIX DEBUG_VM_PREFIX "(%s#:%d)\t"format, __BASE_FILE__, __LINE__, ##__VA_ARGS__)
+#define QEMU_PT_PRINTF_DEBUG(format, ...)       qemu_log_mask(LOG_KAFL, QEMU_PT_PRINT_PREFIX DEBUG_VM_PREFIX "(%s#:%d)\t"format, __BASE_FILE__, __LINE__, ##__VA_ARGS__)
+
+#define QEMU_PT_PRINTF(PREFIX, format, ...)     qemu_log(QEMU_PT_PRINT_PREFIX PREFIX format "\n", ##__VA_ARGS__)
+#define QEMU_PT_DEBUG(PREFIX, format, ...)      qemu_log_mask(LOG_KAFL, QEMU_PT_PRINT_PREFIX PREFIX format "\n", ##__VA_ARGS__)
+#define QEMU_PT_PRINTF_DBG(PREFIX, format, ...) qemu_log_mask(LOG_KAFL, QEMU_PT_PRINT_PREFIX PREFIX "(%s#:%d)\t"format, __BASE_FILE__, __LINE__, ##__VA_ARGS__)
+#endif // NYX_TRACELOG
 #else
 #define debug_printf(format, ...) 
 #define debug_fprintf(fd, format, ...) 
